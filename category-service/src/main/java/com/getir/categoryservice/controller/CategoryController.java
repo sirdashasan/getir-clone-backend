662a565b1732f8e7a50979f2f2b24ce3577e0762
sirdashasan <sirdashasan@hotmail.com>
2025-04-22 17:13:06 +0300

    feat: add CategoryController

---
+++ category-service/src/main/java/com/getir/categoryservice/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.getir.categoryservice.controller;
+
+import com.getir.categoryservice.dto.CategoryRequest;
+import com.getir.categoryservice.dto.CategoryResponse;
+import com.getir.categoryservice.service.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryResponse> getAll() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/slug/{slug}")
+    public ResponseEntity<CategoryResponse> getBySlug(@PathVariable String slug) {
+        return ResponseEntity.ok(categoryService.getCategoryBySlug(slug));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create( @RequestBody CategoryRequest request) {
+        return new ResponseEntity<>(categoryService.createCategory(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> update(@PathVariable UUID id, @RequestBody CategoryRequest request) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
