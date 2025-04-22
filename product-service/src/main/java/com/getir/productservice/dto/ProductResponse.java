49780ac1b5751314bed8e130757ca5581829e727
sirdashasan <sirdashasan@hotmail.com>
2025-04-22 11:31:38 +0300

    feat: add ProductResponse DTO

---
+++ product-service/src/main/java/com/getir/productservice/dto/ProductResponse.java
@@ -0,0 +1,24 @@
+package com.getir.productservice.dto;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+    private UUID id;
+    private String nameTr;
+    private String nameEn;
+    private String slug;
+    private String categorySlug;
+    private String subcategoryTr;
+    private String subcategoryEn;
+    private Double price;
+    private Double oldPrice;
+    private String quantity;
+    private String image;
+}
