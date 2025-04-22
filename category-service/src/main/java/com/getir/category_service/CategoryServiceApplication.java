1bcb2c1b1927cc4858944e390987755f23e0427a
sirdashasan <sirdashasan@hotmail.com>
2025-04-22 16:31:25 +0300

    feat: initialize category-service with Spring Boot and environment config

---
+++ category-service/src/main/java/com/getir/category_service/CategoryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.getir.category_service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load(); //
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+
+}
