249d3f61012bd0ba3e2108a5b8d50633f53922a1
sirdashasan <sirdashasan@hotmail.com>
2025-04-22 16:50:58 +0300

    feat: add CategoryRepository

---
+++ category-service/src/main/java/com/getir/categoryservice/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.getir.categoryservice.repository;
+
+import com.getir.categoryservice.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Category findBySlug(String slug);
+}
