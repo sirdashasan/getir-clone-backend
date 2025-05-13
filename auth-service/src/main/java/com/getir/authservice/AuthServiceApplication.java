d2040d260593531e405b1ef41a904f465e288614
sirdashasan <sirdashasan@hotmail.com>
2025-05-13 14:59:51 +0300

    feat: initialize auth-service with Spring Boot, Docker and API Gateway integration

--- auth-service/src/main/java/com/getir/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/getir/authservice/AuthServiceApplication.java
@@ -1,18 +1,17 @@
-package com.getir.categoryservice;
+package com.getir.authservice;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CategoryServiceApplication {
-
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 
 		Dotenv dotenv = Dotenv.load();
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		SpringApplication.run(CategoryServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
