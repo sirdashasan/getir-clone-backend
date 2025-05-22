3cde81aca31dec894fec41fadddb5c5ffdff8628
sirdashasan <sirdashasan@hotmail.com>
2025-05-22 15:41:34 +0300

    feat: add StockMapper

---
+++ stock-service/src/main/java/com/getir/stockservice/mapper/StockMapper.java
@@ -0,0 +1,36 @@
+package com.getir.stockservice.mapper;
+
+import com.getir.stockservice.dto.ProductCreatedEvent;
+import com.getir.stockservice.dto.StockResponse;
+import com.getir.stockservice.entity.Stock;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockMapper {
+
+    public Stock toEntity(ProductCreatedEvent event) {
+        return Stock.builder()
+                .productId(event.getProductId())
+                .nameTr(event.getNameTr())
+                .nameEn(event.getNameEn())
+                .categorySlugTr(event.getCategorySlugTr())
+                .categorySlugEn(event.getCategorySlugEn())
+                .subcategorySlugTr(event.getSubcategorySlugTr())
+                .subcategorySlugEn(event.getSubcategorySlugEn())
+                .availableQuantity(event.getQuantity())
+                .build();
+    }
+
+    public StockResponse toResponse(Stock stock) {
+        return StockResponse.builder()
+                .productId(stock.getProductId())
+                .nameTr(stock.getNameTr())
+                .nameEn(stock.getNameEn())
+                .categorySlugTr(stock.getCategorySlugTr())
+                .categorySlugEn(stock.getCategorySlugEn())
+                .subcategorySlugTr(stock.getSubcategorySlugTr())
+                .subcategorySlugEn(stock.getSubcategorySlugEn())
+                .availableQuantity(stock.getAvailableQuantity())
+                .build();
+    }
+}
