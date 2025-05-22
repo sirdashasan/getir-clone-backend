7dfcd8ba363e0c974dc418ec082c46cd5ff7f509
sirdashasan <sirdashasan@hotmail.com>
2025-05-22 15:47:31 +0300

    feat: add StockRepository

---
+++ stock-service/src/main/java/com/getir/stockservice/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.getir.stockservice.repository;
+
+import com.getir.stockservice.entity.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, String> {
+}
